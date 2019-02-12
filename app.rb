a248e9a0ea6d423719c3a9c234231ec36effdd75
Oscar Bernardini <bernardini687@gmail.com>
2019-02-12 22:48:30 +0100

    Can add recipes with all attributes

--- app.rb
+++ app.rb
@@ -2,30 +2,38 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry-byebug'
 require 'better_errors'
-require_relative 'cookbook'
-require_relative 'recipe'
 set :bind, '0.0.0.0'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path(__dir__)
 end
 
+require_relative 'cookbook'
+require_relative 'recipe'
+
 get '/' do # <- Router part
   @recipes = Cookbook.new('recipes.csv').all # <- Controller part
   erb :index # <- View is in views/index.erb
-end
-
-post '/create' do
-  cookbook = Cookbook.new('recipes.csv')
-  recipe = Recipe.new(params[:name].capitalize, params[:description].capitalize)
-  cookbook.add(recipe)
-  redirect to '/'
 end
 
 get '/new' do
   erb :new
 end
 
-get '/team/:username' do
-  "The username is #{params[:username]}" # <- params is {"username"=>"testing"}
+post '/recipes' do
+  cookbook = Cookbook.new('recipes.csv')
+  recipe = Recipe.new(
+    params[:name].capitalize,
+    params[:blurb].capitalize,
+    params[:gravity],
+    params[:preptime]
+  )
+  cookbook.add(recipe)
+  redirect to '/'
+end
+
+get '/recipes/:index' do
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv')) # <- Understand...
+  cookbook.remove(params[:index].to_i)
+  redirect to '/'
 end
